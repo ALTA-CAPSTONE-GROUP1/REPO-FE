6bb6ab08cd509109bb254013057261de9c6a973b
projack14 <zakaria.zrs14@gmail.com>
2023-05-11 01:48:22 +0700

    feat component layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { FC, ReactNode } from "react";
+import { NavbarAdmin, NavbarUser } from "./Navbar";
+
+interface Props {
+  children: ReactNode;
+}
+
+export const Layout: FC<Props> = (props) => {
+  const { children } = props;
+  return (
+    <div className="h-screen dark:bg-slate-900">
+      <NavbarAdmin />
+
+      {children}
+    </div>
+  );
+};
