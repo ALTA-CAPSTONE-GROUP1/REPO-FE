dec2a7c2c27436ff70daadc44748aca7e6dd7308
projack14 <zakaria.zrs14@gmail.com>
2023-05-11 18:36:08 +0700

    feat update user, chore table user  to card component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,22 +8,18 @@ interface Props {
 export const LayoutAdmin: FC<Props> = (props) => {
   const { children } = props;
   return (
-<<<<<<< HEAD
     <div className="h-full bg-white pb-20">
       <NavbarAdmin />
       {children}
     </div>
   );
 };
+
 export const Layout: FC<Props> = (props) => {
   const { children } = props;
   return (
-    <div className="h-screen bg-white">
-      <NavbarAdmin />
-=======
-    <div data-theme="cupcake" className="h-screen">
+    <div className="h-screen dark:bg-slate-900">
       <NavbarUser />
->>>>>>> 0e8045ce691bd5600820b6838b3bdedc89ec3d2b
       {children}
     </div>
   );
