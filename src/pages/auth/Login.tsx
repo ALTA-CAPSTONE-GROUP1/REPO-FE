7614fc6e5a8694f0b1583bf5a50407881605b78c
projack14 <zakaria.zrs14@gmail.com>
2023-05-13 13:53:45 +0700

    feat intration login user and admin

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,9 +1,72 @@
-import { FC } from "react";
-import { Link } from "react-router-dom";
+import { FC, FormEvent, useState, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Swal from "sweetalert2";
+import axios from "axios";
+import * as z from "zod";
+
 import { Input } from "@/components/Input";
 import { RedButton } from "@/components/Button";
 
-export const Login: FC = () => {
+const schema = z.object({
+  id: z.string().min(1, { message: "Users ID is required" }),
+  password: z.string().min(4, { message: "Password is 4 minimum character" }),
+});
+
+type Schema = z.infer<typeof schema>;
+
+function Login() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [, setCookie] = useCookies(["token", "id"]);
+  const navigate = useNavigate();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Schema>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<Schema> = (data) => {
+    setLoading(true);
+    axios
+      .post("login", data)
+      .then((res) => {
+        const { data, message } = res.data;
+        const isAdmin = data.id;
+        Swal.fire({
+          icon: "success",
+          title: "Success",
+          text: message,
+          showCancelButton: false,
+        }).then((result) => {
+          if (result.isConfirmed) {
+            setCookie("token", data.token, { path: "/" });
+            setCookie("id", data.id, { path: "/" });
+            if (isAdmin) {
+              navigate("/admin"); // jika user adalah admin, arahkan ke halaman admin
+            } else {
+              navigate("/user"); // jika user bukan admin, arahkan ke halaman user
+            }
+          }
+          console.log(data);
+        });
+      })
+      .catch((error) => {
+        const { data } = error.response;
+        Swal.fire({
+          icon: "error",
+          title: "Failed",
+          text: data.message,
+          showCancelButton: false,
+        });
+      })
+      .finally(() => setLoading(false));
+  };
+
   return (
     <div
       className="hero min-h-screen"
@@ -12,7 +75,10 @@ export const Login: FC = () => {
       }}
     >
       <div className="flex hero-overlay bg-opacity-60 justify-center items-center ">
-        <form className="flex flex-col m-6 p-8 bg-white rounded-md w-full md:w-fit h-fit">
+        <form
+          className="flex flex-col m-6 p-8 bg-white rounded-md w-full md:w-fit h-fit"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <img src="/images/Logo2.png" alt="" />
           <div className="mt-3">
             <h3 className="font-bold text-2xl text-black">Login</h3>
@@ -20,19 +86,40 @@ export const Login: FC = () => {
               Welcome back. Enter your credentials to access your account
             </h3>
             <div className="mt-5 w-full">
-              <label className="font-semibold text-md text-black">
+              <label htmlFor="id" className="font-semibold text-md text-black">
                 User ID
               </label>
-              <Input placeholder="Enter Your ID" id="input-user-id" />
+              <Input
+                register={register}
+                name="id"
+                id="input-user-id"
+                placeholder="Enter Your ID"
+                error={errors.id?.message}
+              />
             </div>
             <div className="mt-5 w-full">
-              <label className="font-semibold text-md text-black">
+              <label
+                htmlFor="password"
+                className="font-semibold text-md text-black"
+              >
                 Password
               </label>
-              <Input placeholder="Enter Your Password" id="input-user-id" />
+              <Input
+                register={register}
+                name="password"
+                placeholder="Enter Your Password"
+                id="input-user-id"
+                type="password"
+                error={errors.password?.message}
+              />
             </div>
             <div className="mt-5">
-              <RedButton label="Login" id="button-login" type="submit" />
+              <RedButton
+                label="Login"
+                id="button-login"
+                type="submit"
+                disabled={loading}
+              />
             </div>
             <div className="flex flex-col mt-5 md:justify-center md:items-center text-md text-black">
               <p>
@@ -61,4 +148,5 @@ export const Login: FC = () => {
       </div>
     </div>
   );
-};
+}
+export default Login;
